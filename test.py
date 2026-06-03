d4cacbdd2c92499fa5b2aec324f52d92e471ca68
Sydney <sydneyb31@me.com>
2026-06-02 21:30:25 -0400

    test game retrieval and selection
    Signed-off-by: Sydney <sydneyb31@me.com>

--- test.py
+++ test.py
@@ -1,13 +1,38 @@
 import os
 from dotenv import load_dotenv
 from steam_web_api import Steam
+import random
 
 load_dotenv(encoding="utf-16")
 
 KEY = os.environ.get("STEAM_API_KEY")
-print(KEY)
+#print(KEY)
 steam = Steam(KEY)
 
-user = steam.users.get_user_details("76561198271994774")  
 
-print(user)
+def get_games():
+    """Gets the games from steam library."""
+    user = steam.users.get_owned_games("76561198271994774")
+
+    #print(len(user['games']))
+
+    games = []
+
+    for i in range(0, len(user['games'])):
+        games.append(user['games'][i]['name'])
+    return games
+
+
+def spin():
+    """Randomly selects a game from library."""
+    return print(random.choice(get_games()))
+
+def main():
+    get_games()
+
+    spin()
+
+
+if __name__ == "__main__":
+    main()
+
