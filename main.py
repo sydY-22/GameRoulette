a51f661a74cbd5d33bdd6748ea32f222d3fc577e
Sydney <sydneyb31@me.com>
2026-06-07 21:42:03 -0400

    removed some code
    Signed-off-by: Sydney <sydneyb31@me.com>

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ class GameRoulette:
         KEY = os.environ.get("STEAM_API_KEY")
         steam = Steam(KEY)
         all_games = steam.users.get_owned_games("76561198271994774")
-
         all_games_ls = []
 
         for i in range(0, len(all_games['games'])):
@@ -58,14 +57,6 @@ class GameRoulette:
         self.string_var.set(f"PLAY!: {random.choice(games)}")
     
 
-    
-    # def menu(self):
-    #     """Displays the menu."""
-    #     print("1. List Games ")
-    #     print("2. Spin!!!")
-    #     print("3. Exit...")
-    
-
 
 def main():
     print("Welcome to Game Roulette!!!")
@@ -83,10 +74,6 @@ def main():
                        background=GREEN, foreground=WHITE, font=("bold", 30))
     button.pack(pady=(0, 20))
 
-    # canvas:
-    #canvas = tk.Canvas(width=400, height=400, bg=ORANGE, highlightthickness=0)
-    #canvas.create_image(500, 500, image=roulette_img)
-
     # show roulette image
     roulette_img = tk.PhotoImage(file="roulette-img2.png")
     label2 = tk.Label(pick.window, image=roulette_img)
@@ -95,18 +82,6 @@ def main():
 
     # run the app:
     pick.window.mainloop()
-
-    # while True:
-    #     pick.menu()
-    #     prompt = input("Please choose options 1-3: ")
-
-    #     if prompt == '1':
-    #         pick.list_games()
-    #     elif prompt == '2':
-    #         pick.spin()
-    #         print()
-    #     else:
-    #         return False
         
 
 
