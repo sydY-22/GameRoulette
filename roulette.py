f863883e645a7bde9c9537df7553cf03074548d4
Sydney <sydneyb31@me.com>
2026-06-09 22:21:09 -0400

    add code to another file
    Signed-off-by: Sydney <sydneyb31@me.com>

---
+++ roulette.py
@@ -0,0 +1,84 @@
+import os
+from dotenv import load_dotenv
+from steam_web_api import Steam
+import random
+import tkinter as tk
+from tkinter import ttk
+from tkinter.ttk import *
+
+load_dotenv(encoding="utf-16")
+
+# color palettes:
+GREEN = "#0A7C6E"
+YELLOW = "#F59E0B"
+ORANGE = "#FF6B35"
+WHITE = "#FAFAFA"
+
+class GameRoulette:
+
+    # main window:
+    window = tk.Tk()
+    window.title('Game Roulette!')
+    window.config(padx=40, pady=40, bg=ORANGE)
+    window.geometry('1000x850')
+
+    string_var = tk.StringVar(value='Game!')
+
+
+    
+    def get_games(self):
+        """Retrieve the games from steam library."""
+        KEY = os.environ.get("STEAM_API_KEY")
+        steam = Steam(KEY)
+        all_games = steam.users.get_owned_games("76561198271994774")
+        all_games_ls = []
+
+        for i in range(0, len(all_games['games'])):
+            all_games_ls.append(all_games['games'][i]['name'])
+        return all_games_ls
+    
+    def list_games(self):
+        """List all the games."""
+        games = self.get_games()
+
+        for game in games:
+            print(f"{game}")
+        print()
+    
+    def spin(self):
+        """Select a game randomly."""
+        games = self.get_games()
+
+        return print(f"PLAY!: {random.choice(games)}")
+    
+    def button_func(self):
+        """Randomly select a game on button press."""
+        games = self.get_games()
+        self.string_var.set(f"PLAY!: {random.choice(games)}")
+    
+
+
+def main():
+    print("Welcome to Game Roulette!!!")
+    pick = GameRoulette()
+
+
+
+    # display the game
+    label = tk.Label(master=pick.window, text='label', textvariable=pick.string_var, 
+                     background=YELLOW, foreground=GREEN, font=("bold", 35))
+    label.pack(pady=(0, 20))
+
+    # spin button
+    button = tk.Button(master=pick.window, text='SPIN!', command=pick.button_func, 
+                       background=GREEN, foreground=WHITE, font=("bold", 30))
+    button.pack(pady=(0, 20))
+
+    # show roulette image
+    roulette_img = tk.PhotoImage(file="roulette-img2.png")
+    label2 = tk.Label(pick.window, image=roulette_img)
+    label2.pack(pady=(0, 20))
+    
+
+    # run the app:
+    pick.window.mainloop()
